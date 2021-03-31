8c1e46c1a9b14314193f9d5ce9b6c4b975254a79
Chris Bellairs <c.m.bellairs@gmail.com>
2021-03-31 15:01:15 +0100

    Created task to make introduction and print a tree to the terminal.

---
+++ Tree/tree.java
@@ -0,0 +1,21 @@
+public class Tree {
+  public static void main(String[] args) {
+    //This task will introduce the user and print a tree
+
+    System.out.println("Hi there, I'm Chris and I'm learning to code Java.'");
+
+    System.out.println("I'm going to plant a tree today");
+
+    System.out.println(" ");
+
+    System.out.println("   **  ");
+
+    System.out.println("  **** ");
+
+    System.out.println(" ****** ");
+
+    System.out.println("   **  ");
+
+    System.out.println("   **  ");
+  }
+}
